d2f37cd40c0f737034b0ad773dd72964fb48d980
Jianhong <stjianhweng@gamil.com>
2018-11-20 19:24:16 -0500

    fixed format

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,55 +1,51 @@
 //Get the dataTable from friends.js
-var dataTable=require('../data/friends')
-console.log("log the dataTable to check the access:\n "+ JSON.stringify(dataTable))
+var dataTable = require('../data/friends')
+console.log("log the dataTable to check the access:\n " + JSON.stringify(dataTable))
 // ===============================================================================
 // ROUTING
 // ===============================================================================
 
-module.exports = function(app) {
+module.exports = function (app) {
   // API GET Requests
   // Below code handles when users "visit" a page.
   // In each of the below cases when a user visits a link
   // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
   // ---------------------------------------------------------------------------
 
-  app.get("/api/friends", function(req, res) {
+  app.get("/api/friends", function (req, res) {
     res.json(dataTable);
   });
 
   // API POST Requests
-  app.post("/api/friends", function(req, res) {
+  app.post("/api/friends", function (req, res) {
     // this is the data of the current user that need to post to the dataarray 
- var currentUser=req.body;
- var matchIndex=0;
- //comparison logics
- //for loop to compare each data in the dataTable 
- for (var i=0;i<dataTable.length;i++){
-  var currentScore=currentUser.scores;
-   var tableScore=dataTable[i].scores;
-   var sumDifference=0;
-   //declare a variable for compare the sumDifference
-  var matching=51;
-   //for loop to compare each result of the question of current user to the existing user
-   for (var j=0; j <currentScore.length;j++){
-     //this will check the different between each index in the score array
-    sumDifference += Math.abs(parseInt(currentScore[j]) - parseInt(tableScore[j]))
+    var currentUser = req.body;
+    var matchIndex = 0;
+    //comparison logics
+    //for loop to compare each data in the dataTable 
+    for (var i = 0; i < dataTable.length; i++) {
+      var currentScore = currentUser.scores;
+      var tableScore = dataTable[i].scores;
+      var sumDifference = 0;
+      //declare a variable for compare the sumDifference
+      var matching = 51;
+      //for loop to compare each result of the question of current user to the existing user
+      for (var j = 0; j < currentScore.length; j++) {
+        //this will check the different between each index in the score array
+        sumDifference += Math.abs(parseInt(currentScore[j]) - parseInt(tableScore[j]))
 
-   }//end of the score comaparison
-if (sumDifference < matching)
-        {
-            matching = sumDifference;
-            matchIndex = i;
-        
-            console.log("new match score is " + matching + " and the index in friendTable is " + matchIndex);
-        }
- 
+      } //end of the score comaparison
+      if (sumDifference < matching) {
+        matching = sumDifference;
+        matchIndex = i;
 
+        console.log("new match score is " + matching + " and the index in friendTable is " + matchIndex);
+      }
+    } // end of the for loop mehtod
 
- }// end of the for loop mehtod
- 
- //this push the newform to the array of data, at the end of comparison to prevent return of self
- dataTable.push(currentUser)
-//return the data in the dataTable for the best match
-res.json(dataTable[matchIndex])
-  });//end of the post function
-};// end of the  export function
+    //this push the newform to the array of data, at the end of comparison to prevent return of self
+    dataTable.push(currentUser)
+    //return the data in the dataTable for the best match
+    res.json(dataTable[matchIndex])
+  }); //end of the post function
+}; // end of the  export function
--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 // DEPENDENCIES
 var express = require("express");
 //Tells node that we are creating an "express" server
@@ -8,18 +7,16 @@ var app = express();
 var PORT = process.env.PORT || 8000;
 
 // Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+  extended: true
+}));
 app.use(express.json());
-
-
 // ROUTER
 // The below points our server to a series of "route" files.
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
-
-
 // LISTENER
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log("App listening on PORT: http://localhost:" + PORT);
 });
